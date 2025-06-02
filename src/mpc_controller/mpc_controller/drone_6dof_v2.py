1721da4761fe267bdb6a3f3ad65ed288a587c763
JJayU <jakub.junkiert@gmail.com>
2025-06-02 12:06:39 +0000

    Fixed comments

--- src/mpc_controller/mpc_controller/drone_6dof_v2.py
+++ src/mpc_controller/mpc_controller/drone_6dof_v2.py
@@ -5,14 +5,15 @@ from acados_template import AcadosModel
 def export_drone_6dof_model():
     model_name = "drone_6dof"
     
-    # Parametry
-    g = 9.81  # przyspieszenie grawitacyjne [m/s^2]
-    m = 0.028   # masa drona [kg]
-    l = 0.046  # ramię quadrotora [m]
-    k = 0.00089468 # współczynnik momentu reakcyjnego
-    J = SX([[1.657171e-5, 0, 0], [0, 1.657171e-5, 0], [0, 0, 2.9261652e-5]])  # macierz bezwładności
+    # Params
+    g = 9.81 
+    m = 0.028  
+    l = 0.046 
+    k = 0.00089468 
+    ct = 0.15 
+    J = SX([[1.657171e-5, 0, 0], [0, 1.657171e-5, 0], [0, 0, 2.9261652e-5]]) 
     
-    # Zmienne stanu
+    # State variables
     px = SX.sym('px')
     py = SX.sym('py')
     pz = SX.sym('pz')
@@ -29,7 +30,7 @@ def export_drone_6dof_model():
     x = vertcat(px, py, pz, vx, vy, vz, phi, theta, psi, wx, wy, wz)
     xdot = SX.sym('xdot', x.size1())
     
-    # Wejścia sterujące (ciągi od silników)
+    # Control inputs
     T1 = SX.sym('T1')
     T2 = SX.sym('T2')
     T3 = SX.sym('T3')
@@ -37,10 +38,10 @@ def export_drone_6dof_model():
     
     u = vertcat(T1, T2, T3, T4)
     
-    # Siła nośna
-    T = (T1 + T2 + T3 + T4) * 0.15 
+    # Thrust
+    T = (T1 + T2 + T3 + T4) * ct
     
-    # Macierz rotacji
+    # Rotational matrix 
     R = SX.zeros(3, 3)
     R[0, 0] = SX.cos(psi) * SX.cos(theta)
     R[0, 1] = SX.cos(psi) * SX.sin(theta) * SX.sin(phi) - SX.sin(psi) * SX.cos(phi)
@@ -52,11 +53,11 @@ def export_drone_6dof_model():
     R[2, 1] = SX.cos(theta) * SX.sin(phi)
     R[2, 2] = SX.cos(theta) * SX.cos(phi)
     
-    # Dynamika translacji
+    # Translation dynamics
     dp = vertcat(vx, vy, vz)
     dv = (1/m) * (mtimes(R, vertcat(0, 0, T)) - vertcat(0, 0, m*g))
     
-    # Dynamika rotacji
+    # Rotational dynamics
     tau_x = l * (T1 + T4 - T2 - T3) 
     tau_y = l * (T3 + T4 - T1 - T2) 
     tau_z = k * (T2 + T4 - T1 - T3)
@@ -64,7 +65,7 @@ def export_drone_6dof_model():
     
     w = vertcat(wx, wy, wz)
     
-    # Macierz skew-symmetric dla wektora prędkości kątowej
+    #
     w_skew = SX.zeros(3, 3)
     w_skew[0, 1] = -wz
     w_skew[0, 2] = wy
@@ -75,13 +76,13 @@ def export_drone_6dof_model():
     
     dw = mtimes(inv(J), (tau - mtimes(w_skew, mtimes(J, w))))
     
-    # Połączone równania stanu
+    # State equations
     x_dot = vertcat(dp, dv, wx, wy, wz, dw[0], dw[1], dw[2])
     
-    # Definicja modelu Acados
+    # Acados model definition
     model = AcadosModel()
     model.f_expl_expr = x_dot
-    model.f_impl_expr = x_dot - xdot  # Poprawiona definicja implikowanego modelu
+    model.f_impl_expr = x_dot - xdot
     model.x = x
     model.xdot = xdot
     model.u = u
